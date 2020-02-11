73a42c7daed78e742781515e403e1f01e366da7d
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-02-10 20:00:36 -0500

    chore: promesas

---
+++ TypeScript/Prueba/src/Promesas.ts
@@ -0,0 +1,32 @@
+//---------------------------------------- Promesas (procesos asincronos)
+//-------------Problema del promise se arregla con ES6
+
+let prom = new Promise( function( resolve, reject){
+
+    setTimeout( () =>{
+        console.log("Promesa terminada");
+
+        //--- Si termina bien
+        resolve();
+
+
+        //---- si no funciona
+        //reject();
+    }, 1500)
+
+})
+
+console.log("Paso 1");
+
+
+prom.then( function(){
+    console.log("Ejecutarme cuando termine bien");
+}, 
+
+    function(){
+        console.error("Ejecutarme si todo sale  mal");
+    }
+
+)
+
+console.log("Paso2");
