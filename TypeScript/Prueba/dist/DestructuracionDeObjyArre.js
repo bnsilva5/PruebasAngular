44bf9e7073ad56e3fbed43ab74a2e528e9617e2b
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-02-10 19:58:41 -0500

    chore: Destructuracion de objetos y arreglos

---
+++ TypeScript/Prueba/dist/DestructuracionDeObjyArre.js
@@ -0,0 +1,32 @@
+//--------------------------Destructuracion de objestos y arreglos
+//------------------Objeto
+let guitar = {
+    marca: "Jackson",
+    cuerdas: 6,
+    modelo: "Jackson-V"
+};
+//-- Destructuracion del objeto
+let { marca, cuerdas, modelo } = guitar;
+console.log(cuerdas, marca, modelo);
+//-------------------- Arreglos
+let band = ["Mick", "Nikki", "Vince", "Tommy"];
+let [guitarra, bajo, voz, bateria] = band;
+console.log(bajo, voz, guitarra, bateria);
+console.log(bajo, guitarra);
+//----------------------------------------
+let cars = ["Mustang", "Corvet", "Audi", "Camaro"];
+let [rojo, azul, verde, morado] = cars;
+console.log(rojo, verde, morado);
+console.log(morado, verde, azul, rojo);
+console.log(rojo);
+//--------------------------------------
+let pantalla = {
+    size: "24",
+    color: "negro",
+    marca2: "Samsung",
+    conexion: "HDMI"
+};
+let { size, color, marca2, conexion } = pantalla;
+console.log(size, color, marca2, conexion);
+console.log(marca2, size, conexion);
+console.log(marca2, conexion);
