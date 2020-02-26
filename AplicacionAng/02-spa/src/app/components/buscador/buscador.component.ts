0b03b2192aaa50e69813f909df4b1b2b652f0238
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-02-26 17:06:49 -0500

    chore: buscador component

---
+++ AplicacionAng/02-spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService } from "../../servicios/heroes.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+
+  @Input() heroes: any[] = [];
+  @Input() termino: string;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    })
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe', idx]);
+  }
+
+}
