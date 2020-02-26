e36441d049ffdd282e08fbcbfba65808a35289e8
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-02-26 17:16:39 -0500

    chore: heroe component

--- AplicacionAng/02-spa/src/app/components/heroe/heroe.component.ts
+++ AplicacionAng/02-spa/src/app/components/heroe/heroe.component.ts
@@ -7,19 +7,19 @@ import { HeroesService } from "../../servicios/heroes.service";
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent{
+export class HeroeComponent {
 
-    heroe:any = [];
+  heroe: any = [];
 
-  constructor( private activatedRouter: ActivatedRoute,
-               private _heroesService: HeroesService
-    ) {
+  constructor(private activatedRouter: ActivatedRoute,
+    private _heroesService: HeroesService
+  ) {
 
-    this.activatedRouter.params.subscribe( params => {
-      this.heroe = this._heroesService.getHeroe( params['id'] );
+    this.activatedRouter.params.subscribe(params => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
       console.log(this.heroe);
     })
 
-   }
+  }
 
 }
